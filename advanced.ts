5fea18c26ee016cb1c05ccccb55f44ad6881c004
calliope-net <141074597+calliope-net@users.noreply.github.com>
2026-03-22 10:18:22 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ advanced.ts
@@ -0,0 +1,189 @@
+
+namespace modem // advanced.ts
+/*
+
+*/ {
+
+    // ========== group="Senden (ASCII Code 32..127)" advanced=true
+
+    //% group="Senden (ASCII Code 32..127)" advanced=true blockGap=8
+    //% block="ASCII Code aus Text %text || Index %index" weight=8
+    //% index.min=0 index.max=9 index.defl=0
+    export function charCodeAt(text: string, index = 0) {
+        return text.charCodeAt(index)
+    }
+
+    // blockId=rs_232_ascii_to_array10
+    //% group="Senden (ASCII Code 32..127)" advanced=true blockGap=8
+    //% block="10 Bit Array aus ASCII Code %ascii_code" weight=3
+    //% ascii_code.min=32 ascii_code.max=127 ascii_code.defl=13
+    export function ascii_to_array10(ascii_code: number): boolean[] {
+        /*
+        Parameter byte: 7 Bit ASCII Code 0..127
+        Return Array: 10 boolean Elemente (wahr/falsch)
+        1 Start, 7 Daten 2^0..2^6, 1 Parität, 1 Stop-Bit
+        negative Logik: 0=wahr und 1=falsch
+        */
+        let parity = 0, bBit: boolean
+        let array_10bit: boolean[] = [true, true, true, true, true, true, true, true, true, true]
+        // [0] 1 START Bit = wahr (Licht an) [1]..[7] 7 Daten Bit 2 ^ 0..2 ^ 6
+        for (let i = 1; i < 7; i++) {
+            if ((ascii_code & 0x01) == 1) { // Bit 2^0 ist 1
+                array_10bit[i] = false
+                parity++
+            }
+            //alle 8 Bit nach rechts schieben  2 ^ 1 wird 2 ^ 0 >> 2 ^ 0 fällt raus
+            ascii_code = ascii_code >> 1
+        }
+        if ((parity & 0x01) == 1) { // Parity ist ungerade
+            // [8] 1 PARTIY Bit:  wenn Anzahl der Einsen ungerade ist, das letzte Bit auf 1 (falsch) setzen, damit eine gerade Anzahl entsteht
+            array_10bit[8] = false
+        }
+        // [9] 1 STOP Bit = falsch (Licht aus)
+        array_10bit[9] = false
+
+        // array_10bit.push(parity % 2 != 0) // [7] das 8. Bit Parity 
+        return array_10bit
+    }
+
+    //% group="Senden (ASCII Code 32..127)" advanced=true blockGap=8
+    //% block="sende %array_10bit" weight=1
+    // array_10bit.shadow=rs_232_ascii_to_array10
+    export function rs232_sende10bit(array_10bit: boolean[]) {
+        /*
+        1 Zeichen senden mit Licht
+        Parameter Array: 10 boolean Elemente (wahr/falsch)
+        1 Startbit, 7 Datenbit, 1 Paritätsbit, 1 Stopbit
+        negative Logik: 0=wahr=Licht an und 1=falsch=aus
+        */
+        let send_pause_ms = input.runningTime() + q_takt_ms
+
+        // 10 Bit aus Array senden: 1 Start, 7 Daten 2 ^ 0..2 ^ 6, 1 Parität, 1 Stop - Bit
+        for (let i = 0; i < array_10bit.length; i++) {
+            sende_1bit(array_10bit[i]) // 7 Datenbits + 1 Paritätsbit
+            /*   if (array_10bit[i])
+                  pins.digitalWritePin(n_pinLED, 1)
+              else
+                  pins.digitalWritePin(n_pinLED, 0) */
+            basic.pause(send_pause_ms - input.runningTime())
+            send_pause_ms += q_takt_ms
+        }
+
+        // STOP Bit verlängern als Pause (Licht aus)
+        basic.pause(send_pause_ms - input.runningTime())
+    }
+
+
+
+
+
+
+    // ========== group="Empfangen (1 Start, 7 Daten 2^0..2^6, 1 Parität, 1 Stop-Bit)" advanced=true
+
+
+    //% group="Empfangen (1 Start, 7 Daten 2^0..2^6, 1 Parität, 1 Stop-Bit)" advanced=true blockGap=8
+    //% block="Text-Zeichen aus ASCII Code %ascii_code" weight=8
+    //% ascii_code.min=32 ascii_code.max=127 ascii_code.defl=32
+    export function fromCharCode(ascii_code: number) {
+        return String.fromCharCode(ascii_code)
+    }
+
+    //% group="Empfangen (1 Start, 7 Daten 2^0..2^6, 1 Parität, 1 Stop-Bit)" advanced=true blockGap=8
+    //% block="ASCII Code aus %array_10bit" weight=3
+    export function ascii_from_array10(array_10bit: boolean[]): number {
+        /*
+        Parameter Array: 10 boolean Elemente (wahr/falsch)
+        1 Start, 7 Daten 2^0..2^6, 1 Parität, 1 Stop-Bit
+        negative Logik: 0=wahr und 1=falsch
+        Return byte: 7 Bit ASCII Code 0..127
+        Return bei Fehler -1 .. -4
+        */
+        let ascii_code = 0
+        let parity = 0
+        if (array_10bit.length < 10) { // 10 Bit im Array boolean[]
+            // Array Länge muss >=10 sein; bei Fehler: Return - 1
+            ascii_code = -1
+        } else if (!array_10bit[0]) {
+            // [0] Start Bit muss true sein; bei Fehler: Return - 2
+            ascii_code = -2
+        } else if (array_10bit[9]) {
+            // [9] Stop Bit muss false sein; bei Fehler: Return - 4
+            ascii_code = -2
+        } else {
+            for (let i = 1; i <= 7; i++) {
+                // index [1]..[7]; 7 Daten-Bits (negative Logik)
+                if (!array_10bit[i]) {
+                    parity++
+                    // 2^(1-1) = 2^0 = 1 bis 2^6 = 64; addiert 1, 2, 4, 8, 16, 32, 64 bei false
+                    ascii_code += 2 ** (i - 1)
+                }
+            }
+            if (!array_10bit[8]) {
+                // [8] Parity Bit auswerten (negative Logik)
+                parity++
+            }
+            if ((parity & 0x01) == 1) {
+                // Parity muss gerade sein; bei Fehler: Return - 3
+                ascii_code = -3
+            }
+        }
+        return ascii_code
+    }
+
+    //% group="Empfangen (1 Start, 7 Daten 2^0..2^6, 1 Parität, 1 Stop-Bit)" advanced=true blockGap=8
+    //% block="empfange 10 Bit Array" weight=1
+    export function rs232_empfange10bit(): boolean[] {
+        let empf_pause_ms: number
+        let array_10bit: boolean[] = []
+
+        // warten auf START Bit (Fototransistor hell)
+        while (!empfange1bit()) {
+            //empf_pause_ms = input.runningTime() + n_takt_ms * n_startBitTime // 0.5 oder 0.45 zwischen Beginn Startbit und einlesen
+            if (n_empf_abbrechen)
+                break // kann vom Bedienfeld abgebrochen werden
+            /*  if (n_takt_ms >= 50)
+                 basic.pause(10) // ohne Pause funktioniert das Abbruch Ereignis nicht (z.B. Knopf A halten)
+             else
+                 basic.pause(n_takt_ms * 0.1) */
+        }
+        if (!n_empf_abbrechen) {
+            // Daten empfangen
+            // Startbit nach einer halben Taktzeit einlesen
+            empf_pause_ms = input.runningTime() + q_takt_ms * q_start_bit_time // 0.5 oder 0.45 zwischen Beginn Startbit und einlesen
+
+            for (let i = 0; i < 10; i++) {
+                basic.pause(empf_pause_ms - input.runningTime())
+                array_10bit.push(empfange1bit()) // Lichtschranke abfragen
+                // einen Takt warten, trifft das nächste Bit in der Mitte
+                empf_pause_ms += q_takt_ms
+                if (n_empf_abbrechen)
+                    break // kann vom Bedienfeld abgebrochen werden
+            }
+        }
+        return array_10bit
+    }
+
+
+
+
+
+
+
+
+
+    // ========== group="ASCII" advanced=true
+
+    //% group="Kommentar" advanced=true blockGap=8
+    //% block="// %text" weight=6
+    export function comment(text: string): void { }
+
+
+
+    // ========== group="Funktionen" advanced=true blockGap=8
+
+    //% group="Funktionen" advanced=true
+    //% block="%i0 zwischen %i1 und %i2" weight=5
+    export function between(i0: number, i1: number, i2: number): boolean {
+        return (i0 >= i1 && i0 <= i2)
+    }
+} // advanced.ts
---
+++ modem.ts
@@ -0,0 +1,137 @@
+
+//% color=#002050 icon="\uf093" block="RS-232 Modem" weight=15
+namespace modem // modem.ts
+/* 
+    C16: fischertechnik Fototransistor 36134 mit Pull-Up Widerstand 100 k / Calliope v2
+    C17: fischertechnik Lichtschranken-LED 9V 0.01A 162135
+    oder
+    P2: Fototransistor ohne Pull-Up Widerstand
+    P1: Lichtschranken-LED
+    Takt zwischen 2 Calliope: 50ms / zum BT Smart Controller: 400ms
+    https://de.wikipedia.org/wiki/RS-232
+    https://calliope-net.github.io/rs232-e41/
+    https://calliope-net.github.io/rs232-e41/rs232.png
+
+    Lutz Elßner März 2026
+*/ {
+    let q_pin_led: DigitalPin = DigitalPin.P1
+    let q_pin_fototransistor: AnalogPin = AnalogPin.P2 // dunkel~860 / hell~20 / Calliope v2
+    let q_helligkeit: number = 150
+    export let q_takt_ms: number = 400 // Takt zwischen 2 Calliope: 20ms / zum BT Smart Controller: 400ms
+    export let q_start_bit_time: number = 0.45 // 0.5 oder 0.45 zwischen Beginn Startbit und einlesen
+    export let n_empf_abbrechen: boolean = false // warten auf Startbit beim Empfang abbrechen
+
+
+
+    // ========== group="asynchrone serielle Datenübertragung mit Licht"
+
+    //% group="asynchrone serielle Datenübertragung mit Licht"
+    //% block="Pins: LED %pin_led Fototransistor %pin_fototransistor || Helligkeit < %helligkeit" weight=5
+    //% pin_led.defl=DigitalPin.P1 pin_fototransistor.defl=AnalogPin.P2 helligkeit.defl=150
+    export function set_pins(pin_led: DigitalPin, pin_fototransistor: AnalogPin, helligkeit = 150) {
+        q_pin_led = pin_led
+        q_pin_fototransistor = pin_fototransistor
+        q_helligkeit = helligkeit
+    }
+
+    //% group="asynchrone serielle Datenübertragung mit Licht"
+    //% block="Takt: %takt_ms ms || Startbit Time %start_bit_time" weight=4
+    //% takt_ms.min=20 takt_ms.max=1000 takt_ms.defl=400
+    //% start_bit_time.defl=0.5
+    export function set_takt(takt_ms: number, start_bit_time = 0.5) {
+        q_takt_ms = takt_ms
+        q_start_bit_time = start_bit_time
+    }
+
+
+
+    // ========== group="Senden (LED)"
+
+
+    //% group="Senden (LED)"
+    //% block="sende 1 Zeichen ASCII Code %ascii_code" weight=6
+    //% ascii_code.min=32 ascii_code.max=127 ascii_code.defl=13
+    export function sende_code(ascii_code: number) {
+        rs232_sende10bit(ascii_to_array10(ascii_code))
+    }
+
+    //% group="Senden (LED)"
+    //% block="sende Text Zeile %text mit ↵ ENTER" weight=4
+    export function sende_text_mit13(text: string) {
+        /*
+        Parameter text: string; mit_log: boolean
+        sendet jedes Zeichen aus text + chr(13)
+        ASCII 13: ENTER / CR 'Carriage Return'
+        Parameter mit_log = True:
+        protokolliert jedes Zeichen in Konsole
+        */
+        for (let i = 0; i < text.length; i++) {
+            sende_code(text.charCodeAt(i))
+        }
+        sende_code(13)
+    }
+
+    export function sende_1bit(bit: boolean) {
+        if (bit)
+            pins.digitalWritePin(q_pin_led, 1)
+        else
+            pins.digitalWritePin(q_pin_led, 0)
+    }
+
+
+
+    // ========== group="Empfangen (Fototransistor)"
+
+    //% group="Empfangen (Fototransistor)"
+    //% block="empfange 1 Zeichen ASCII Code (oder Fehlercode)" weight=6
+    export function empfange_1zeichen() {
+        n_empf_abbrechen = false
+        return ascii_from_array10(rs232_empfange10bit()) // ASCII-Code oder Fehler-Code -1 -2 -3 -4
+    }
+
+    //% group="Empfangen (Fototransistor)"
+    //% block="empfange Text Zeile bis ↵ ENTER" weight=4
+    export function empfange_text_bis13() {
+        /*
+        wartet auf (Licht) Zeichen
+        hängt jedes empfangene Zeichen an text an
+        endet erst nach Code 13 / CR / Zeilenende
+        kein Abbruch bei Fehler, Fehlercode im Text
+        Parameter mit_log = True:
+        protokolliert jedes Zeichen in Konsole
+        */
+        n_empf_abbrechen = false
+        let empf_text = ""
+        while (!n_empf_abbrechen) {
+            // 1 Zeichen empfangen (10 Licht-Zeichen)
+            let array_10bit = rs232_empfange10bit()
+            let ascii_code = ascii_from_array10(array_10bit)
+
+            if (ascii_code == 13) {
+                // beendet die Empfangs-Schleife; 13 / CR / Zeilenende
+                break
+            }
+            if (between(ascii_code, 32, 127))
+                empf_text += String.fromCharCode(ascii_code)
+            else
+                empf_text += "|" + ascii_code + "|"
+        }
+        return empf_text
+    }
+
+    //% group="Empfangen (Fototransistor)"
+    //% block="empfange 1 Bit (warten auf Startbit)" weight=3
+    export function empfange1bit(): boolean {
+        // hell ist true, analoger Wert < 150
+        return pins.analogReadPin(q_pin_fototransistor) < q_helligkeit
+    }
+
+    //% group="Empfangen (Fototransistor)"
+    //% block="Empfangen abbrechen || %abbrechen" weight=2
+    //% abbrechen.shadow=toggleYesNo abbrechen.defl=1
+    export function empfang_abbrechen(abbrechen: boolean) {
+        n_empf_abbrechen = abbrechen
+    }
+
+
+} // modem.ts
